d66557bc7b99f59d90beb82c1e075a69adcf5aa1
luanfv <luanfrancavieira@gmail.com>
2024-04-21 18:09:52 -0300

    refactor: code

--- src/application/request-ride.service.spec.ts
+++ src/application/request-ride.service.spec.ts
@@ -1,30 +1,3 @@
-import { RideEntity } from '../domain/ride.entity';
-import { RideRepository } from '../infra/repository/ride.repository';
-import { AccountRepository } from '../infra/repository/account.repository';
-
-export type RequestRideServiceDto = {
-	passengerId: string,
-	fromLat: number,
-	fromLong: number,
-	toLat: number,
-	toLong: number,
-};
-
-export class RequestRideService {
-	constructor (readonly accountRepository: AccountRepository, readonly rideRepository: RideRepository) {}
-	
-	async execute (input: RequestRideServiceDto): Promise<string> {
-		const account = await this.accountRepository.getAccountById(input.passengerId);
-		if (!account) throw new Error('Conta não encontrada');
-		if (!account.isPassenger) throw new Error('Conta não é de um passageiro');
-		const hasActiveRide = await this.rideRepository.hasActiveRideByPassengerId(input.passengerId);
-		if (hasActiveRide) throw new Error('Passageiro já esta em uma corrida');
-		const ride = RideEntity.create(input.passengerId, input.fromLat, input.fromLong, input.toLat, input.toLong);
-		await this.rideRepository.saveRide(ride);
-		return ride.rideId;
-	}
-}
-
 describe('RequestRideService unit tests', () => {
 	describe('WHEN account does not found', () => {
 		it.todo('SHOULD throw error with message: "Conta não encontrada"');
--- src/application/request-ride.service.ts
+++ src/application/request-ride.service.ts
@@ -15,7 +15,8 @@ export class RequestRideService {
 	
 	async execute (input: RequestRideServiceDto): Promise<string> {
 		const account = await this.accountRepository.getAccountById(input.passengerId);
-		if (!account?.isPassenger) throw new Error('Conta não é de um passageiro');
+		if (!account) throw new Error('Conta não encontrada');
+		if (!account.isPassenger) throw new Error('Conta não é de um passageiro');
 		const hasActiveRide = await this.rideRepository.hasActiveRideByPassengerId(input.passengerId);
 		if (hasActiveRide) throw new Error('Passageiro já esta em uma corrida');
 		const ride = RideEntity.create(input.passengerId, input.fromLat, input.fromLong, input.toLat, input.toLong);
